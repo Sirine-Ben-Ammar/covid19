c968ea7fc158c713f2b41c34ff664feedfb8f585
Sirine-Ben-Ammar <sirinebenammar22@gmail.com>
2020-06-25 22:40:59 +0200

    firstcommit

---
+++ src/swingcrud/Region.java
@@ -0,0 +1,51 @@
+package swingcrud;
+
+/**
+ *
+ * @author Sirine
+ */
+public class Region {
+
+    String name, longitude, latitude;
+    int id, pop, confirmes, morts;
+
+    public Region(int id, String name, String longitude, String latitude, int pop, int confirmes, int morts) {
+        this.id = id;
+        this.name = name;
+        this.longitude = latitude;
+        this.latitude = latitude;
+        this.pop = pop;
+        this.confirmes = confirmes;
+        this.morts = morts;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public int getPop() {
+        return pop;
+    }
+
+    public int getConfirmes() {
+        return confirmes;
+    }
+
+    public int getMorts() {
+        return morts;
+    }
+
+}
